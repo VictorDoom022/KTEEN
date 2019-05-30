cd79ad8bc168162c9cf91bef3999aa40b36e663c
TuanHee <TuanHee0726@github.com>
2019-05-30 23:02:10 +0800

    Create index.php

---
+++ stallManagementSystem/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<title>Staff login</title>
+</head>
+<body>
+	<nav class="navbar-xs navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="adminlogin.html">KTEEN | Staff Login</a>
+		</div>
+	</nav>
+</body>
+</html>
