bd21d189a635f2ced173bded136400f4426d179f
TuanHee <TuanHee0726@github.com>
2019-06-16 16:37:49 +0800

    Update index.php

--- adminSystem/index.php
+++ adminSystem/index.php
@@ -73,96 +73,89 @@ if(isset($_POST['edit'])){
             </div>
             <div class="col-md-10">
 			<div class="card">
-                        <div class="row">
-                        	<?php
-                        		$sql = "SELECT ID, stall_name, owner_name, email, contact_no FROM stall";
-                        		$result = $conn -> query($sql);
+                <div class="row">
+                	<?php
+                		$sql = "SELECT ID, stall_name, owner_name, email, contact_no FROM stall";
+                		$result = $conn -> query($sql);
 
-                        		if($result -> num_rows >0){
-                        			while ($row = $result -> fetch_assoc()){
-                        				$id = $row['ID'];
-                        				$stallName = $row['stall_name'];
-                        				$ownerName = $row['owner_name'];
-                        				$email = $row['email'];
-                        				$phoneNo = $row['contact_no'];
-                        			
-                        	?>
-                            <div class="card" style="width: 18rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title" name="item[]"><?php echo $stallName; ?></h5>	
-                                        <ul class="list-group list-group-flush">
-                                        	<li class="list-group-item"><?php echo $ownerName; ?></li>
-                                        	<li class="list-group-item"><?php echo $email; ?></li>
-                                        	<li class="list-group-item"><?php echo $phoneNo; ?></li>
-                                        	
-                                        </ul>
-                                </div>
-                                <div class="card-body">
-                                	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#edit-<?php echo $id; ?>">
-  										Edit
-									</button>
-                       
-                                	<a href="adminmain.php?del=<?php echo $id; ?>" class="card-link" onclick="return ComfirmDelete()">Delete</a>
+                		if($result -> num_rows >0){
+                			while ($row = $result -> fetch_assoc()){
+                				$id = $row['ID'];
+                				$stallName = $row['stall_name'];
+                				$ownerName = $row['owner_name'];
+                				$email = $row['email'];
+                				$phoneNo = $row['contact_no'];
+                	?>
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title" name="item[]"><?php echo $stallName; ?></h5>	
+                            <ul class="list-group list-group-flush">
+                            	<li class="list-group-item"><?php echo $ownerName; ?></li>
+                            	<li class="list-group-item"><?php echo $email; ?></li>
+                            	<li class="list-group-item"><?php echo $phoneNo; ?></li>
+                            </ul>
+                        </div>
+                        <div class="card-body">
+                        	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#edit-<?php echo $id; ?>">
+									Edit
+							</button>
+               
+                        	<a href="adminmain.php?del=<?php echo $id; ?>" class="card-link" onclick="return ComfirmDelete()">Delete</a>
 
-                                	<div class="modal fade" id="edit-<?php echo $id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  										<div class="modal-dialog" role="document">
-    										<div class="modal-content">
-      											<div class="modal-header">
-       	 											<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          												<span aria-hidden="true">&times;</span>
-        											</button>
-      											</div>
-      											<div class="modal-body">
-        											<form method="post" action="adminmain.php">
-								    					<input type="hidden" value="<?php echo $id; ?>" name="id">
-								            			<div class="form-group">
-								    						<label for="stallName">Stall Name</label>
-								    						<input type="text" class="form-control" value="<?php echo $stallName; ?>" name="stallName" id="stallName">
-								  						</div>
-								  						<div class="form-group">
-								    						<label for="exampleInputEmail1">Owner Name</label>
-								    						<input type="text" class="form-control" value="<?php echo $ownerName; ?>" name="ownerName" id="ownerName">
-								  						</div>
-								  						<div class="form-group">
-								    						<label for="exampleInputEmail1">Email address</label>
-								    						<input type="email" class="form-control" value="<?php echo $email; ?>" name="email" id="email">
-								  						</div>
-								  						<div class="form-group">
-								    						<label for="exampleInputPassword1">Phone No</label>
-								    						<input type="number" class="form-control" value="<?php echo $phoneNo; ?>" name="phoneNo" id="phoneNo">
-								  						</div>
-								  						<div class="form-group">
-								    						<label for="exampleInputPassword1">Password</label>
-								    						<input type="password" class="form-control" value="<?php echo $password; ?>" name="password" id="password">
-								  						</div>
-								  				</div>
-								  				<div class="modal-footer">
-        											<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        											
-        											<input type="submit" class="btn btn-primary bg-warning" value="edit" name="edit">
-      											</div>
-		
-													</form>
-        												
-        											
-      											
-      										
-    										</div>
-  										</div>
+                        	<div class="modal fade" id="edit-<?php echo $id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+									<div class="modal-dialog" role="document">
+									<div class="modal-content">
+											<div class="modal-header">
+	 											<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+  												<span aria-hidden="true">&times;</span>
+											</button>
+											</div>
+											<div class="modal-body">
+											<form method="post" action="adminmain.php">
+						    					<input type="hidden" value="<?php echo $id; ?>" name="id">
+						            			<div class="form-group">
+						    						<label for="stallName">Stall Name</label>
+						    						<input type="text" class="form-control" value="<?php echo $stallName; ?>" name="stallName" id="stallName">
+						  						</div>
+						  						<div class="form-group">
+						    						<label for="exampleInputEmail1">Owner Name</label>
+						    						<input type="text" class="form-control" value="<?php echo $ownerName; ?>" name="ownerName" id="ownerName">
+						  						</div>
+						  						<div class="form-group">
+						    						<label for="exampleInputEmail1">Email address</label>
+						    						<input type="email" class="form-control" value="<?php echo $email; ?>" name="email" id="email">
+						  						</div>
+						  						<div class="form-group">
+						    						<label for="exampleInputPassword1">Phone No</label>
+						    						<input type="number" class="form-control" value="<?php echo $phoneNo; ?>" name="phoneNo" id="phoneNo">
+						  						</div>
+						  						<div class="form-group">
+						    						<label for="exampleInputPassword1">Password</label>
+						    						<input type="password" class="form-control" value="<?php echo $password; ?>" name="password" id="password">
+						  						</div>
+						  				</div>
+						  				<div class="modal-footer">
+											<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+											
+											<input type="submit" class="btn btn-primary bg-warning" value="edit" name="edit">
+											</div>
+											</form>
 									</div>
-                                </div>			
-                            </div> 
-                            <?php
-                            	}
-                        	}
-                            ?>  
-                                                
-                            </div>
-                            
-                        </div>
-                        </div>
+									</div>
+							</div>
+                        </div>			
+                    </div> 
+                    <?php
+                    	}
+                	}
+                    ?>  
+                                        
                     </div>
+                    
+                </div>
+                </div>
+            </div>
 		</div>
 	</div>
 </body>
