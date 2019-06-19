73de7b39392aaa22e5e28cbc9b8eb571314673c8
TuanHee <TuanHee0726@github.com>
2019-06-20 00:22:44 +0800

    Update menu.php

--- stallManagementSystem/menu.php
+++ stallManagementSystem/menu.php
@@ -149,19 +149,15 @@ if (isset($_GET['dfid'])) {
 
 	<link rel="stylesheet" href="../css/kteen_style.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	
-	<script src="https://kit.fontawesome.com/baa8fb89d5.js"></script>
-	<script type="text/javascript">
-		
-	</script>
 
+	<script src="https://kit.fontawesome.com/baa8fb89d5.js"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	
 	<title>KTEEN</title>
 </head>
-<body class="bg-light">
+<body class="bg-light" onload="filter()">
 	<nav class="k-top-nav navbar navbar-expand-lg navbar-light pl-4 col-10 bg-white border-bottom">
 		<span class="navbar-brand h1 mb-0 col"><i class="fas fa-bars d-inline-flex mr-2"></i>Menu</span>
 		<ul class="navbar-nav px-4">
@@ -192,13 +188,13 @@ if (isset($_GET['dfid'])) {
                         <span class="d-none d-md-inline-flex ml-3">Menu</span>
                     </a>
                 </li>
-                <li class="nav-item  w-100 mb-1">
+                <li class="nav-item w-100 mb-1">
                     <a href="report.php" class="nav-link w-100">
                         <i class="far fa-chart-bar d-inline-flex"></i>
                         <span class="d-none d-md-inline-flex ml-3">Report</span>
                     </a>
                 </li>
-                <li class="nav-item  w-100 mb-1">
+                <li class="nav-item w-100 mb-1">
                     <a href="employee.php" class="nav-link w-100">
                         <i class="fas fa-home d-inline-flex"></i>
                         <span class="d-none d-md-inline-flex ml-3">Employee</span>
@@ -308,9 +304,7 @@ if (isset($_GET['dfid'])) {
 						</div>
 					</div>
 				</div>
-				<div id="menu">
-					<?php include('menutable.php'); ?>
-				</div>
+				<div id="menu"></div>
 			</main>
 		</div>
 	</div>
