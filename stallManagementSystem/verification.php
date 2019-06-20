ea831843eda3cdadfc5652a7e6639c80252cdaa0
TuanHee <TuanHee0726@github.com>
2019-06-20 21:48:13 +0800

    Update verification.php

--- stallManagementSystem/verification.php
+++ stallManagementSystem/verification.php
@@ -7,9 +7,6 @@ if(isset($_POST['login'])){
     $email = $_POST['email'];
     $password = $_POST['password'];
     $password = md5($password);
-    //$sql = "UPDATE stall set password = '$password' WHERE email = '$email'";
-    //$sql = "SELECT stall_name, email, password FROM stall WHERE email = '$email' AND password = '$password'";
-    //$result = $conn->query($sql);
 
     if ($stmt = $conn->prepare("SELECT ID, stall_name, email, password FROM stall WHERE email = ? AND password = ?")){
         $stmt->bind_param("ss", $email, $password);
