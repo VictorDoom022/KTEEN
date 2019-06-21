4b12bc4307e31f880b7a084ee6c00b8a4c29ed28
TuanHee <TuanHee0726@github.com>
2019-06-21 20:55:48 +0800

    Update menutable_test.php

--- stallManagementSystem/menutable_test.php
+++ stallManagementSystem/menutable_test.php
@@ -1,8 +1,5 @@
 <?php 
-if(!isset($_SESSION['kteen_stallID'])){
-    session_start();
-}
-
+session_start();
 include('../config.php');
 $filterCategory = "";
 if(isset($_GET['category'])){
@@ -66,44 +63,54 @@ if (isset($_GET['k'])) {
                 <form action="menu_test.php" method="post" enctype="multipart/form-data">
                     <input type="hidden" name="id" value="<?php echo $food_ID; ?>">
                     <div class="modal-body">
-                        <div class="form-row">
-                            <div class="col form-group">
-                                <label for="name">Food Name</label>
-                                <input type="text" name="name" id="name" class="form-control" placeholder="Food Name" value="<?php echo $food_name; ?>">
-                            </div>
-                            <div class="col form-group">
-                                <label for="category">Category</label>
-                                <select name="category_ID" id="category" class="form-control" required>
-                                    <option value="">Select a category</option>
-                                    <?php
-                                    $group = "SELECT * FROM category";
-                                    $category = $conn -> query($group);
-                                    if($category->num_rows > 0){
-                                        while($row = $category -> fetch_assoc()){
-                                    ?>
-                                    <option value="<?php echo $row['ID'] ?>" <?php if($row['ID'] == $category_ID){ echo 'selected'; }?> ><?php echo $row['name']; ?></option>
-                                    <?php
-                                        }
-                                    }
-                                    ?>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="col-7 form-group">
-                                <label>Image</label>
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="customFile" name="fileToUpload" required>
-                                    <label class="custom-file-label" for="customFile">Choose file</label>
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="w-100 k-image-edit">
+                                    <img src="../images/<?php echo $image; ?>" class="k-edit-img img-fluid mx-auto" style="width: 100%;height: 200px;align-self: center;vertical-align: center;">
+                                    <input type="file" name="" class="k-btn-edit-img">
                                 </div>
                             </div>
-                            <div class="col form-group">
-                                <label for="price">Price</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text">RM</div>
+                            <div class="col-md-7">
+                                <div class="form-row">
+                                    <div class="col form-group">
+                                        <label for="name">Food Name</label>
+                                        <input type="text" name="name" id="name" class="form-control" placeholder="Food Name" value="<?php echo $food_name; ?>">
                                     </div>
-                                    <input type="number" name="price" id="price" min="0" step="0.05" class="form-control" placeholder="Price" value="<?php echo $price; ?>" required>
+                                    <div class="col form-group">
+                                        <label for="category">Category</label>
+                                        <select name="category_ID" id="category" class="form-control" required>
+                                            <option value="">Select a category</option>
+                                            <?php
+                                            $group = "SELECT * FROM category";
+                                            $category = $conn -> query($group);
+                                            if($category->num_rows > 0){
+                                                while($row = $category -> fetch_assoc()){
+                                            ?>
+                                            <option value="<?php echo $row['ID'] ?>" <?php if($row['ID'] == $category_ID){ echo 'selected'; }?> ><?php echo $row['name']; ?></option>
+                                            <?php
+                                                }
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="col-7 form-group">
+                                        <label>Image</label>
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="customFile" name="fileToUpload" accept="image/*" required>
+                                            <label class="custom-file-label" for="customFile">Choose file</label>
+                                        </div>
+                                    </div>
+                                    <div class="col form-group">
+                                        <label for="price">Price</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">RM</div>
+                                            </div>
+                                            <input type="number" name="price" id="price" min="0" step="0.05" class="form-control" placeholder="Price" value="<?php echo $price; ?>" required>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
