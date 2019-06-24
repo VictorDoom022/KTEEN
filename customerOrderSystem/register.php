8a2a06e48d8b65e71d97c4e56de35df0c89d641e
TuanHee <TuanHee0726@github.com>
2019-06-24 20:56:44 +0800

    Update register.php

--- customerOrderSystem/register.php
+++ customerOrderSystem/register.php
@@ -27,82 +27,30 @@
 							REGISTER
 						</h2>
 						<form class="needs-validation" novalidate>
-						  <div class="form-row">
-						    <div class="col-md-4 mb-3">
-						      <label for="validationCustom01">First name</label>
-						      <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
-						      <div class="valid-feedback">
-						        Looks good!
-						      </div>
-						    </div>
-						    <div class="col-md-4 mb-3">
-						      <label for="validationCustom02">Last name</label>
-						      <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
-						      <div class="valid-feedback">
-						        Looks good!
-						      </div>
-						    </div>
-						    <div class="col-md-4 mb-3">
-						      <label for="validationCustomUsername">Username</label>
-						      <div class="input-group">
-						        <div class="input-group-prepend">
-						          <span class="input-group-text" id="inputGroupPrepend">@</span>
-						        </div>
-						        <input type="text" class="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" required>
-						        <div class="invalid-feedback">
-						          Please choose a username.
-						        </div>
-						      </div>
-						    </div>
-						  </div>
-						  <div class="form-row">
-						    <div class="col-md-6 mb-3">
-						      <label for="validationCustom03">City</label>
-						      <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
-						      <div class="invalid-feedback">
-						        Please provide a valid city.
-						      </div>
-						    </div>
-						    <div class="col-md-3 mb-3">
-						      <label for="validationCustom04">State</label>
-						      <input type="text" class="form-control" id="validationCustom04" placeholder="State" required>
-						      <div class="invalid-feedback">
-						        Please provide a valid state.
-						      </div>
-						    </div>
-						    <div class="col-md-3 mb-3">
-						      <label for="validationCustom05">Zip</label>
-						      <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
-						      <div class="invalid-feedback">
-						        Please provide a valid zip.
-						      </div>
-						    </div>
-						  </div>
-						  <div class="form-group">
-						    <div class="form-check">
-						      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-						      <label class="form-check-label" for="invalidCheck">
-						        Agree to terms and conditions
-						      </label>
-						      <div class="invalid-feedback">
-						        You must agree before submitting.
-						      </div>
-						    </div>
-						  </div>
-						  <button class="btn btn-primary" type="submit">Submit form</button>
-						</form>
-
-						
-						<form>
 							<div class="form-group">
-								<label for="exampleFormControlInput1">Email</label>
-								<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+								<label for="email">Email</label>
+								<input type="email" class="form-control" id="email" placeholder="name@example.com" required>
+								<div class="valid-feedback">
+								    Looks good!
+								</div>
 							</div>
-							<div class="form-group row">
-								<label for="password">Password</label>
-								<input type="password" id="password" name="password" placeholder="Password" class="form-control">
+							<div class="form-row">
+								<div class="col-md-6 mb-3">
+									<label for="validationCustom03">Password</label>
+									<input type="password" class="form-control" id="validationCustom03" placeholder="Password" required>
+									<div class="invalid-feedback">
+										invalid pasword
+									</div>
+								</div>
+								<div class="col-md-6 mb-3">
+									<label for="validationCustom03">Confirm Password</label>
+									<input type="password" class="form-control" id="validationCustom03" placeholder="Confirm Password" required>
+									<div class="invalid-feedback">
+										invalid password
+									</div>
+								</div>
 							</div>
-							<input type="submit" name="" value="LOGIN" class="btn btn-dark rounded-0 float-right">
+							<input type="submit" name="" value="REGISTER" class="btn btn-dark rounded-0 float-right">
 						</form>
 					</div>
 					<div class="card-footer p-0 border-0">
