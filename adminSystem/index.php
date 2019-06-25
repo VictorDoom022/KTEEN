b6f8bbee810022911bdda3e6770bb69dc6a9841f
TuanHee <TuanHee0726@github.com>
2019-06-25 17:48:48 +0800

    Update index.php

--- adminSystem/index.php
+++ adminSystem/index.php
@@ -35,58 +35,61 @@ if(isset($_POST['edit'])){
 }
 
 if(isset($_POST['add'])){
-		$stallName = $_POST['stallName'];
-		$ownerName = $_POST['ownerName'];
-		$NRIC = $_POST['NRIC'];
-		$email = $_POST['email'];
-		$phoneNo = $_POST['phoneNo'];
-		$password = $_POST['password'];
-		$image= $stallName.'.jpg';
-		$password = md5($password);
-		
-		$sql = "INSERT into stall(stall_name, owner_name,NRIC,image ,email, contact_no, password, status) values ('$stallName','$ownerName','$NRIC','$image','$email','$phoneNo','$password', '1')";
-		$result = $conn->query($sql);
+	$stallName = $_POST['stallName'];
+	$ownerName = $_POST['ownerName'];
+	$NRIC = $_POST['NRIC'];
+	$email = $_POST['email'];
+	$phoneNo = $_POST['phoneNo'];
+	$password = $_POST['password'];
+	$image= $stallName.'.jpg';
+	$password = md5($password);
+	
+	$sql = "INSERT into stall(stall_name, owner_name,NRIC,image ,email, contact_no, password, status) values ('$stallName','$ownerName','$NRIC','$image','$email','$phoneNo','$password', '1')";
+	$result = $conn->query($sql);
 
-		$target_dir = "../images/stall/"; //folder name
-		$target_file = $target_dir . $image; //type of image
-		$uploadOk = 1;
-		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-		// Check if image file is a actual image or fake image
-		if(isset($_POST["add"])) {
-		    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-		    if($check !== false) {
-		        echo "File is an image - " . $check["mime"] . ".";
-		        $uploadOk = 1;
-		    } else {
-		        echo "File is not an image.";
-		        $uploadOk = 0;
-		    }
-		}
-		
-		// Check file size
-		if ($_FILES["fileToUpload"]["size"] > 500000) {
-		    echo "Sorry, your file is too large.";
-		    $uploadOk = 0;
-		}
-		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		&& $imageFileType != "gif" ) {
-		    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-		    $uploadOk = 0;
-		}
-		// Check if $uploadOk is set to 0 by an error
-		if ($uploadOk == 0) {
-		    echo "Sorry, your file was not uploaded.";
-		// if everything is ok, try to upload file
-		} else {
-		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-		    } else {
-		        echo "Sorry, there was an error uploading your file.";
-		    }
-		}
-	
-	
+	$target_dir = "../images/stall/"; //folder name
+	$target_file = $target_dir.$image; //type of image
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	// Check if image file is a actual image or fake image
+    if(isset($_POST["add"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg") {
+        echo "Sorry, only JPG file are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        } else {
+			echo "Sorry, there was an error uploading your file.";
+        }
+	}
+	$conn->close();
+	//header('location: menu.php'); 
 }
 	
 
@@ -166,7 +169,7 @@ if(isset($_POST['add'])){
 							<div class="col form-group">
 								<label>Image</label>
 								<div class="custom-file">
-									<input type="file" class="custom-file-input" id="customFile" name="fileToUpload" accept="image/*" required>
+									<input type="file" class="custom-file-input" id="customFile" name="fileToUpload" required>
 									<label class="custom-file-label" for="customFile">Choose file</label>
 								</div>
 							</div>
