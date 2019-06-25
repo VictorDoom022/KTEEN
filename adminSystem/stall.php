e94535966c92428c8490e328310f9e918eb7b4ec
TuanHee <TuanHee0726@github.com>
2019-06-25 22:09:07 +0800

    Update stall.php

--- adminSystem/stall.php
+++ adminSystem/stall.php
@@ -10,81 +10,118 @@ if(isset($_GET['k'])){
 
 <div class="row">
 	<?php 
-	$sql = "SELECT ID, stall_name, owner_name, image, email, contact_no FROM stall".$keyword;
+	$sql = "SELECT ID, stall_name, owner_name, image, email, contact_no, status FROM stall".$keyword;
 	$result = $conn -> query($sql);
 
 	if($result ->num_rows >0){
 		while ($row = $result -> fetch_assoc()){
-			$id = $row['ID'];
-			$stallName = $row['stall_name'];
-			$ownerName = $row['owner_name'];
+			$stall_id = $row['ID'];
+			$stall_name = $row['stall_name'];
+			$owner_name = $row['owner_name'];
 			$email = $row['email'];
 			$phoneNo = $row['contact_no'];
 			$image = $row['image'];
+			$status = $row['status'];
 	?>
-	<div class="col-12 col-md-4 col-lg-3 p-2">
-		<div class="k-card card k-hover-shadow h-100">
-			<img src="../images/stall/<?php echo $image; ?>" style="width: 100%;height: 200px;align-self: center;vertical-align: center;">
-	        <div class="card-body">
-	            <h5 class="card-title" name="item[]"><?php echo $stallName; ?></h5>	
-	            <ul class="list-group list-group-flush">
-	            	<li class="list-group-item"><?php echo $ownerName; ?></li>
-	            	<li class="list-group-item"><?php echo $email; ?></li>
-	            	<li class="list-group-item"><?php echo $phoneNo; ?></li>
-	            </ul>
-	        </div>
-	        <div class="card-footer bg-white">
-	        	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#edit-<?php echo $id; ?>">
-						Edit
-				</button>
-
-	        	<a href="index.php?del=<?php echo $id; ?>" class="card-link" onclick="return ComfirmDelete()">Delete</a>
-
-	        	<div class="modal fade" id="edit-<?php echo $id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-						<div class="modal-dialog" role="document">
-						<div class="modal-content">
-								<div class="modal-header">
-										<h5 class="modal-title" id="exampleModalLabel">Edit stall</h5>
-								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-										<span aria-hidden="true">&times;</span>
-								</button>
-								</div>
-								<form method="post" action="index.php">
-								<div class="modal-body">
-			    					<input type="hidden" value="<?php echo $id; ?>" name="id">
-			            			<div class="form-group">
-			    						<label for="stallName">Stall Name</label>
-			    						<input type="text" class="form-control" value="<?php echo $stallName; ?>" name="stall_name" id="stallName">
-			  						</div>
-			  						<div class="form-group">
-			    						<label for="exampleInputEmail1">Owner Name</label>
-			    						<input type="text" class="form-control" value="<?php echo $ownerName; ?>" name="owner_name" id="ownerName">
-			  						</div>
-			  						<div class="form-group">
-			    						<label for="exampleInputEmail1">Email address</label>
-			    						<input type="email" class="form-control" value="<?php echo $email; ?>" name="email" id="email">
-			  						</div>
-			  						<div class="form-group">
-			    						<label for="exampleInputPassword1">Phone No</label>
-			    						<input type="number" class="form-control" value="<?php echo $phoneNo; ?>" name="contact_no" id="phoneNo">
-			  						</div>
-			  						<div class="form-group">
-			    						<label for="exampleInputPassword1">Password</label>
-			    						<input type="password" class="form-control" value="<?php echo $password; ?>" name="password" id="password">
-			  						</div>
-			  				</div>
-			  				<div class="modal-footer">
-								<button class="btn btn-secondary bg-dark" data-dismiss="modal">Close</button>
-								
-								<input type="submit" class="btn btn-primary bg-warning bg-dark" value="Submit" name="edit">
-								</div>
-								</form>
+	<div class="col-12 col-md-4 p-2">
+		<a href="#view<?php echo $stall_id; ?>" data-toggle="modal" style="text-decoration: none;color: black;">
+			<div class="k-card card k-hover-shadow h-100">
+				<div class="row no-gutters">
+					<div class="col-md-4">
+						<img class="rounded-circle p-2" src="../images/stall/<?php echo $image; ?>" style="width: 100%;height: 100%;align-self: center;vertical-align: center;">
+					</div>
+					<div class="col-md-8">
+						<div class="card-body">
+							<h5 class="card-title">
+								<?php echo $stall_name ?>
+							</h5>
+							<div class="card-text">
+								<?php echo $owner_name; ?>
+							</div>
+							<div class="card-text">
+								<?php if ($status == "1") { ?>
+									<small class="text-success">opening</small>
+								<?php }else{ ?>
+									<small class="text-danger">closing</small>
+								<?php } ?>
+							</div>
 						</div>
 					</div>
 				</div>
-	        </div>			
-	    </div>
+			</div>
+		</a>
 	</div>
+
+	<div class="modal fade" id="view<?php echo $stall_id; ?>" tabindex="-1" role="modal">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header p-0" style=";position: relative;">
+                	<div class="row p-0 m-0" style="height: 200px;overflow: hidden;">
+                		<img src="../images/stall/<?php echo $image; ?>" style="width: 100%;height: 500px;align-self: center;vertical-align: center;opacity: 0.7;">
+                	</div>
+                    <button type="button" class="btn btn-dark rounded-circle" data-dismiss="modal" aria-label="Close" style="position: absolute;top: -20px;right: 20px;z-index: 3;">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="index.php" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="id" value="<?php echo $stall_id; ?>">
+                    <div class="modal-body" style="position: relative;">
+                    	<img class="rounded-circle" src="../images/stall/<?php echo $image ?>" style="position: absolute;top: -110px;left: 10px;height: 100px;width: 100px;">
+                        <div class="form-row">
+							<div class="col form-group">
+								<label for="stallName">Stall Name</label>
+								<input type="text" class="form-control" placeholder="Enter stall name" name="stallName" id="stallName">
+							</div>
+						</div>
+						<div class="form-row">
+							<div class="col form-group">
+								<label for="exampleInputEmail1">Owner Name</label>
+								<input type="text" class="form-control" placeholder="Enter owner's name" name="ownerName" id="ownerName">
+							</div>
+						</div>
+						<div class="form-row">
+							<div class="col form-group">
+								<label for="exampleInputEmail1">NRIC NO</label>
+								<input type="text" class="form-control" placeholder="Enter NRIC" name="NRIC" id="NRIC">
+							</div>
+							<div class="col form-group">
+								<label>Image</label>
+								<div class="custom-file">
+									<input type="file" class="custom-file-input" id="customFile" name="fileToUpload" required>
+									<label class="custom-file-label" for="customFile">Choose file</label>
+								</div>
+							</div>
+						</div>
+						<div class="form-row">
+							<div class="col form-group">
+								<label for="exampleInputEmail1">Email address</label>
+								<input type="email" class="form-control" placeholder="Enter email" name="email" id="email">
+							</div>
+							<div class="col form-group">
+								<label for="exampleInputPassword1">Phone No</label>
+								<input type="number" class="form-control" placeholder="Enter contact number" name="phoneNo" id="phoneNo" required>
+							</div>
+						</div>
+						<div class="form-row">
+							<div class="col form-group">
+								<label for="exampleInputPassword1">Password</label>
+									<input type="password" class="form-control" placeholder="Password" name="password" id="password" required>
+							</div>
+							<div class="col form-group">
+								<label for="exampleInputPassword1">Comfirm Password</label>
+								<input type="password" class="form-control" placeholder="Password" name="Comfirm password" id="password1" required>
+								<div class="invalid-feedback" role="alert" id="validate-status"></div>
+							</div>
+						</div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-sm" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <input type="submit" name="editmenu" class="btn btn-sm btn-warning" value="Edit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 	<?php
 		}
 	}
