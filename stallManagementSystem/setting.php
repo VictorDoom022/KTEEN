138253dac6c0efb9424bea5a5b09e9c3781afc17
TuanHee <TuanHee0726@github.com>
2019-07-15 23:32:04 +0800

    Create setting.php

---
+++ stallManagementSystem/setting.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col-12 card k-card px-0">
+        <div class="card-header bg-white p-0">
+            <div class="row p-0 m-0" style="height: 200px;overflow: hidden;">
+                <img class="edit-image" src="../images/stall/<?php echo $stall_image; ?>" style="width: 100%;height: 500px;align-self: center;vertical-align: center;opacity: 0.7;">
+            </div>
+        </div>
+        <div class="card-body" style="position: relative;">
+            <div class="edit-image rounded-circle border-0" style="position: absolute;top: -60px;left: 60px;background-image: url(../images/stall/owner/<?php echo $owner_image; ?>);background-size: cover;height: 100px;width: 100px;">
+            </div>
+            <div class="pt-5">
+                
+            </div>
+        </div>
+    </div>
+</div>
