dd7dc441ca19d9ac13e5eaf58c151ad4cf74c39b
TuanHee <TuanHee0726@github.com>
2019-07-15 23:31:33 +0800

    Update index.php

--- stallManagementSystem/index.php
+++ stallManagementSystem/index.php
@@ -53,8 +53,7 @@ if($result->num_rows == 1){
             </li>
             <li class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown">
-                    <div class="edit-image rounded-circle border-0" style="position: absolute;transform: translate(-50%, -50%);top: 50%;left: -20px;background-image: url(../images/stall/owner/<?php echo $owner_image; ?>);background-size: cover;height: 40px;width: 40px;">
-                    </div>
+                    <img class="rounded-circle" style="height: 40px;width: 40px;" src="../images/stall/owner/<?php echo $owner_image; ?>">
                     <?php echo $stall_name; ?>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right rounded-0 shadow border-0" aria-labelledby="navbarDropdown">
@@ -122,22 +121,7 @@ if($result->num_rows == 1){
         <div class="row">
             <div class="col-2"></div>
             <main class="col-10 col-md-8 p-4">
-                <div class="row">
-                    <div class="col-12 card k-card px-0">
-                        <div class="card-header bg-white p-0">
-                            <div class="row p-0 m-0" style="height: 200px;overflow: hidden;">
-                                <img class="edit-image" src="../images/stall/<?php echo $stall_image; ?>" style="width: 100%;height: 500px;align-self: center;vertical-align: center;opacity: 0.7;">
-                            </div>
-                        </div>
-                        <div class="card-body" style="position: relative;">
-                            <div class="edit-image rounded-circle border-0" style="position: absolute;top: -60px;left: 60px;background-image: url(../images/stall/owner/<?php echo $owner_image; ?>);background-size: cover;height: 100px;width: 100px;">
-                            </div>
-                            <div class="pt-5">
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
             </main>
             <div class="d-none d-md-flex col-md-2"></div>
         </div>
