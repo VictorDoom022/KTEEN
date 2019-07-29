a55b2eede1c9afa92a4b62a91f119c319ce77120
TuanHee <TuanHee0726@github.com>
2019-07-29 21:01:24 +0800

    Create handle_login.php

---
+++ stallManagementSystem/controller/handle_login.php
@@ -0,0 +1,11 @@
+<?php 
+
+if (isset($_SESSION['kteen_stallID'])) {
+    $ID = $_SESSION['kteen_stallID'];
+    $stall_name = $_SESSION['kteen_stallN'];
+    $owner_image = $_SESSION['kteen_stallOI'];
+}else{
+    echo "<script>window.location.assign('login.php');</script>";
+}
+
+ ?>
