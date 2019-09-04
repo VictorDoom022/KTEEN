4c627abeef0bd6545c0c0e7190ec5b3cde8d8c35
TuanHee <TuanHee0726@github.com>
2019-09-04 23:24:26 +0800

    Update menu.php

--- stall_management_system/menu.php
+++ stall_management_system/menu.php
@@ -9,10 +9,15 @@ include '../process/handle_logout.php';
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="../css/kteen_style.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	<!-- Bootstarp CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+	<script src="https://kit.fontawesome.com/baa8fb89d5.js"></script>
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<title></title>
 </head>
 <body>
@@ -52,7 +57,7 @@ include '../process/handle_logout.php';
 						</div>
 					</div>
 				</div>
-				<?php include 'menu_card.php'; ?>
+				<?php include 'menu_list.php'; ?>
 				<nav aria-label="..." class="row text-right">
 					<ul class="pagination">
 						<li class="page-item disabled">
@@ -78,11 +83,5 @@ include '../process/handle_logout.php';
 			</div>
 		</div>
 	</main>
-
-	<script src="https://kit.fontawesome.com/baa8fb89d5.js"></script>
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
