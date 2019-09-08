5dd729c9fe396c0b58377f4f15d030537e3a637c
TuanHee <TuanHee0726@github.com>
2019-09-08 23:52:59 +0800

    Create handle_add_stall.php

---
+++ process/handle_add_stall.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST['add_stall'])) {
+	$username = $_POST['username'];
+	$owner_name = $_POST['owner_name'];
+	$stall_name = $_POST['stall_name'];
+	$NRIC = $_POST['NRIC'];
+	// $owner_image = $_POST[''];
+	// $stall_image = $_POST[''];
+	$contact_no = $_POST['contact_no'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	
+	// header('location: add_stall.php');
+}
+?>
