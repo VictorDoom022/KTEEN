5737ea0bd0a999a02b82a9ce0ed95538fa77c114
TuanHee <TuanHee0726@github.com>
2019-09-08 20:40:04 +0800

    Update add_stall.php

--- admin_system/add_stall.php
+++ admin_system/add_stall.php
@@ -1,3 +1,8 @@
+<?php 
+session_start();
+include '../process/handle_logout.php';
+include '../process/handle_if_logout.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,36 +23,123 @@
 </head>
 <body>
 	<?php 
+	$site = '';
 	include '../layout/top_nav_admin.php';
 	include '../layout/side_nav_admin.php';
 	?>
 	<main class="container-fluid">
-		<div class="row pt-3">
+		<div class="row py-3">
 			<div class="col-2"></div>
 			<div class="col-10">
 				<div class="k-card card">
-					<div class="card-body">
-						<form>
-							<div class="row">
-								<div class="col-4">
-									<img src="">
-								</div>
-								<div class="col-8">
-									<div class="form-group">
-										<input type="" name="" class="form-control">
-										<?php 
-										echo __dir__ . "<br>";
-										echo __file__ . "<br>";
-										echo __file__ . "<br>";
-										?>
+					<form action="" method="post" enctype="multipart/form-data">
+						<div class="row no-gutters">
+							<div style="position: relative;width: 100%;height: 250px;">
+								<img src="../images/stall_image.png" style="height: 250px; width: 100%;position: absolute;" id="img-stall">
+								<input type="file" name="" id="input-stall-image" required style="position: absolute;width: 100%;height: 100%;opacity: 0;" data-target="#img-stall">
+								<label for="input-stall-image" class="btn btn-light" style="position: absolute;right: 10px;bottom: 5px;"><i class="fas fa-camera"></i></label>
+								<div style="position: absolute;top: 50%;left: 1%;">
+									<div style="position: relative;width: 250px;height: 250px;">
+										<img src="../images/personal.jpg" class="rounded-circle" style="width: 250px;height: 250px;position: absolute;" id="img-owner">
+										<input type="file" name="" id="input-owner-image" data-target="#img-owner" style="width: 250px;height: 250px;position: absolute;border-radius: 50%;opacity: 0;">
+										<label for="input-owner-image" class="btn btn-dark" style="position: absolute;bottom: 10px;right: 20px;z-index: 2;"><i class="fas fa-camera"></i></label>
 									</div>
 								</div>
 							</div>
-						</form>
-					</div>
+						</div>
+						<div class="card-body">
+							<div class="row">
+								<div class="col-md-4">
+									<br>
+									<br>
+									<br>
+									<br>
+								</div>
+								<div class="col-md-8">
+									<div class="card-title h3 border-bottom">
+										Personal Detail
+									</div>
+									<div class="form-group row">
+										<label class="col-md-2 col-form-label">Name</label>
+										<div class="col-md-10">
+											<input type="text" name="" class="form-control">
+										</div>
+									</div>
+									<div class="form-group row">
+										<label class="col-md-2 col-form-label">NRIC</label>
+										<div class="col-md-10">
+											<input type="text" name="" class="form-control">
+										</div>
+									</div>
+									<div class="form-group row">
+										<label class="col-md-2 col-form-label">Contact No</label>
+										<div class="col-md-10">
+											<input type="text" name="" class="form-control">
+										</div>
+									</div>
+									<div class="form-group row">
+										<label class="col-md-2 col-form-label">Email</label>
+										<div class="col-md-10">
+											<input type="text" name="" class="form-control">
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col border-bottom card-title h3">
+									Account
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group row">
+										<label class="col-md-3 col-form-label">ID</label>
+										<div class="col-md-9">
+											<input type="text" name="" class="form-control">
+										</div>
+									</div>
+									<div class="form-group row">
+										<label class="col-md-3 col-form-label">Password</label>
+										<div class="col-md-9">
+											<input type="text" name="" class="form-control">
+										</div>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group row">
+										<label class="col-md-3 col-form-label">Stall Name</label>
+										<div class="col-md-9">
+											<input type="text" name="" class="form-control">
+										</div>
+									</div>
+									<div class="form-group row" style="font-size: 0.82rem;">
+										<label class="col-md-3 col-form-label">Confirm Password</label>
+										<div class="col-md-9">
+											<input type="text" name="" class="form-control">
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col text-right">
+									<button class="btn text-danger">Cancel</button>
+									<input type="submit" class="btn" value="Submit">
+								</div>
+							</div>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
 	</main>
+	<script src="../js/show_input_image.js"></script>
+	<script type="text/javascript">
+		$("#input-stall-image").change(function() {
+			readURL(this);
+		});
+		$("#input-owner-image").change(function() {
+			readURL(this);
+		});
+	</script>
 </body>
 </html>
