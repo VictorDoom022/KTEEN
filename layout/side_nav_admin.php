72dc2d32da5ddbf69e355870a3d434b66b01406a
TuanHee <TuanHee0726@github.com>
2019-09-08 20:54:48 +0800

    Update side_nav_admin.php

--- layout/side_nav_admin.php
+++ layout/side_nav_admin.php
@@ -7,13 +7,21 @@
     </div>
     <div class="k-nav-container h-75">
         <ul class="k-nav nav">
-            <li class="nav-item w-100 mb-1">
-                <a href="index.php" class="nav-link w-100 active">
-                    <i class="fas fa-bars d-inline-flex"></i>
-                    <span class="d-none d-md-inline-flex ml-3">Home</span>
-                </a>
-            </li>
-             
+            <?php if ($site == 'index') { ?>
+                <li class="nav-item w-100 mb-1">
+                    <a href="index.php" class="nav-link w-100 active">
+                        <i class="fas fa-bars d-inline-flex"></i>
+                        <span class="d-none d-md-inline-flex ml-3">Home</span>
+                    </a>
+                </li>
+            <?php }else{ ?>
+                <li class="nav-item w-100 mb-1">
+                    <a href="index.php" class="nav-link w-100">
+                        <i class="fas fa-arrow-left"></i>
+                        <span class="d-none d-md-inline-flex ml-3">Cancel</span>
+                    </a>
+                </li>
+            <?php } ?>
         </ul>
     </div>
 </nav>
