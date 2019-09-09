f914c16f3abfa17741a068993fcb216afb453871
TuanHee <TuanHee0726@github.com>
2019-09-09 21:59:23 +0800

    Update handle_add_stall.php

--- process/handle_add_stall.php
+++ process/handle_add_stall.php
@@ -1,6 +1,13 @@
 <?php
+$username = '';
+$owner_name = '';
+$stall_name = '';
+$NRIC = '';
+$contact_no = '';
+$email = '';
+$password = '';
+
 if (isset($_POST['add_stall'])) {
-	$username = $_POST['username'];
 	$owner_name = $_POST['owner_name'];
 	$stall_name = $_POST['stall_name'];
 	$NRIC = $_POST['NRIC'];
@@ -8,11 +15,23 @@ if (isset($_POST['add_stall'])) {
 	// $stall_image = $_POST[''];
 	$contact_no = $_POST['contact_no'];
 	$email = $_POST['email'];
-	$password = $_POST['password'];
 
-	mkdir('../images/'.$username);
-	mkdir('../images/'.$username.'/menu');
-	mkdir('../images/'.$username.'/employee');
+	if($_POST['password'] == $_POST['confirm_password']){
+		$password = $_POST['password'];
+
+		echo $sql = "SELECT username FROM stall WHERE username = '".$_POST['username']."';";
+		$result = mysqli_query($conn, $sql);
+		if(mysqli_num_rows($result) == 0){
+			$username = $_POST['username'];
+			mkdir('../images/'.$username);
+			mkdir('../images/'.$username.'/menu');
+			mkdir('../images/'.$username.'/staff');
+		}else{
+
+		}
+		
+	}
+	
 	// header('location: add_stall.php');
 }
 ?>
