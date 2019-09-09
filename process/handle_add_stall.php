b9d9340ed331b0c32ea3a2e3d789ecc8e0639472
TuanHee <TuanHee0726@github.com>
2019-09-09 23:18:05 +0800

    Update handle_add_stall.php

--- process/handle_add_stall.php
+++ process/handle_add_stall.php
@@ -1,4 +1,5 @@
 <?php
+include '../config/test_input.php';
 $username = '';
 $owner_name = '';
 $stall_name = '';
@@ -7,31 +8,56 @@ $contact_no = '';
 $email = '';
 $password = '';
 
+$username_valid = '';
+$owner_name_valid = '';
+$stall_name_valid = '';
+$NRIC_valid = '';
+$contact_no_valid = '';
+$email_valid = '';
+$password_valid = '';
+
 if (isset($_POST['add_stall'])) {
-	$owner_name = $_POST['owner_name'];
-	$stall_name = $_POST['stall_name'];
-	$NRIC = $_POST['NRIC'];
+	$owner_name = test_input($_POST['owner_name']);
+	$stall_name = test_input($_POST['stall_name']);
+	$NRIC = test_input($_POST['NRIC']);
 	// $owner_image = $_POST[''];
 	// $stall_image = $_POST[''];
-	$contact_no = $_POST['contact_no'];
-	$email = $_POST['email'];
+	$contact_no = test_input($_POST['contact_no']);
+	$email = test_input($_POST['email']);
+
+	$owner_name_valid = 'is-valid';
+	$stall_name_valid = 'is-valid';
+	$NRIC_valid = 'is-valid';
+	$contact_no_valid = 'is-valid';
+	$email_valid = 'is-valid';
 
 	if($_POST['password'] == $_POST['confirm_password']){
-		$password = $_POST['password'];
+		$password = md5(test_input($_POST['password']));
 
-		echo $sql = "SELECT username FROM stall WHERE username = '".$_POST['username']."';";
+		$password_valid = 'is-valid';
+		$sql = "SELECT username FROM stall WHERE username = '".test_input($_POST['username'])."';";
 		$result = mysqli_query($conn, $sql);
+
 		if(mysqli_num_rows($result) == 0){
-			$username = $_POST['username'];
+			$username = test_input($_POST['username']);
+
+			$username_valid = 'is-valid';
+
+			$sql = "INSERT INTO stall(username, stall_name, owner_name, NRIC, contact_no, email, password, status) VALUES ('$username', '$stall_name', '$owner_name', '$NRIC', 'contact_no', '$email', '$password', '1');";
+			$result = mysqli_query($conn, $sql) or die(mysqli_error());
+
 			mkdir('../images/'.$username);
 			mkdir('../images/'.$username.'/menu');
 			mkdir('../images/'.$username.'/staff');
+			mysqli_close($conn);
+			
+			header('location: index.php');
 		}else{
-
+			mysqli_close($conn);
 		}
 		
+	}else{
+		$password_valid = 'is-invalid';
 	}
-	
-	// header('location: add_stall.php');
 }
 ?>
