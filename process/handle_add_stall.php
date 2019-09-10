153b2e67f4a086aca597c2c465e34baae1b61826
TuanHee <TuanHee0726@github.com>
2019-09-10 23:45:12 +0800

    Update handle_add_stall.php

--- process/handle_add_stall.php
+++ process/handle_add_stall.php
@@ -33,31 +33,32 @@ if (isset($_POST['add_stall'])) {
 
 	if($_POST['password'] == $_POST['confirm_password']){
 		$password = md5(test_input($_POST['password']));
-
 		$password_valid = 'is-valid';
-		$sql = "SELECT username FROM stall WHERE username = '".test_input($_POST['username'])."';";
-		$result = mysqli_query($conn, $sql);
-
-		if(mysqli_num_rows($result) == 0){
-			$username = test_input($_POST['username']);
-
-			$username_valid = 'is-valid';
-
-			$sql = "INSERT INTO stall(username, stall_name, owner_name, NRIC, contact_no, email, password, status) VALUES ('$username', '$stall_name', '$owner_name', '$NRIC', 'contact_no', '$email', '$password', '1');";
-			$result = mysqli_query($conn, $sql) or die(mysqli_error());
-
-			mkdir('../images/'.$username);
-			mkdir('../images/'.$username.'/menu');
-			mkdir('../images/'.$username.'/staff');
-			mysqli_close($conn);
-			
-			header('location: index.php');
-		}else{
-			mysqli_close($conn);
-		}
-		
 	}else{
 		$password_valid = 'is-invalid';
+	}
+
+	$sql = "SELECT username FROM stall WHERE username = '".test_input($_POST['username'])."';";
+	$result = mysqli_query($conn, $sql);
+
+	if(mysqli_num_rows($result) == 0){
+		$username = test_input($_POST['username']);
+
+		$username_valid = 'is-valid';
+	}else{
+		$username_valid = 'is-invalid';
+	}
+
+	if($username_valid == 'is-valid' && $owner_name_valid == 'is-valid' && $stall_name_valid == 'is-valid' && $NRIC_valid == 'is-valid' && $contact_no_valid == 'is-valid' && $email_valid == 'is-valid' && $password_valid == 'is-valid'){
+		$sql = "INSERT INTO stall(username, stall_name, owner_name, NRIC, contact_no, email, password, status) VALUES ('$username', '$stall_name', '$owner_name', '$NRIC', '$contact_no', '$email', '$password', '1');";
+		$result = mysqli_query($conn, $sql) or die(mysqli_error());
+
+		mkdir('../images/'.$username);
+		mkdir('../images/'.$username.'/menu');
+		mkdir('../images/'.$username.'/staff');
+		mysqli_close($conn);
+		
+		header('location: index.php');
 	}
 }
 ?>
