8a615c083a473ca31e250624713ca93ac0e4b5f5
TuanHee <TuanHee0726@github.com>
2019-09-11 22:26:14 +0800

    Update handle_delete_stall.php

--- process/handle_delete_stall.php
+++ process/handle_delete_stall.php
@@ -1,7 +1,15 @@
 <?php 
 
-if (isset(var)) {
-	# code...
-}
+if (isset($_GET['st_u'])) {
+	$stall_username = $_GET['st_u'];
+	$sql = "DELETE FROM stall WHERE username = '$stall_username';";
+	$result = $conn -> query($sql);
+	$conn->close();
 
+	$path = '../images/'.$stall_username;
+	if(!rmdir($path)){
+		echo ("Could not remove $path");
+	}
+	// header("location: index.php");
+}
 ?>
