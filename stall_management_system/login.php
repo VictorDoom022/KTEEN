0f5734b11a45f8c14fb981596c784726903f6c4a
TuanHee <TuanHee0726@github.com>
2019-09-13 00:55:23 +0800

    Update login.php

--- stall_management_system/login.php
+++ stall_management_system/login.php
@@ -27,8 +27,8 @@
 					<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 						<div class="card-body">
 							<div class="form-group">
-								<label for="email">Username</label>
-								<input type="text" name="username" class="form-control" id="email" placeholder="Enter Username" required>
+								<label for="username">Username</label>
+								<input type="text" name="username" class="form-control" id="username" placeholder="Enter Username" required>
 							</div>
 							<div class="form-group">
 								<label for="password">Password</label>
