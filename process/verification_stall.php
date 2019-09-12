180d22427fa2a6d59605b84ff3b64c8524de5d9d
TuanHee <TuanHee0726@github.com>
2019-09-13 00:26:03 +0800

    Update verification_stall.php

--- process/verification_stall.php
+++ process/verification_stall.php
@@ -4,14 +4,14 @@ include("../config/config.php");
 $error = "";
 
 if(isset($_POST['login'])){
-    $email = $_POST['email'];
+    $username = $_POST['username'];
     $password = $_POST['password'];
     $password = md5($password);
 
-    if ($stmt = $conn->prepare("SELECT ID, stall_name, owner_image, username FROM stall WHERE email = ? AND password = ?")){
-        $stmt->bind_param("ss", $email, $password);
+    if ($stmt = $conn->prepare("SELECT ID, username, stall_name, owner_image FROM stall WHERE username = ? AND password = ?")){
+        $stmt->bind_param("ss", $username, $password);
         $stmt->execute();
-        $stmt->bind_result($stall_ID, $stall_name, $owner_image, $username);
+        $stmt->bind_result($stall_ID, $username, $stall_name, $owner_image);
         if ($stmt->fetch()) {
             $_SESSION['kteen_stall_id'] = $stall_ID;
             $_SESSION['kteen_stall_name'] = $stall_name;
@@ -20,7 +20,7 @@ if(isset($_POST['login'])){
             $error = "";
             header('location: index.php');
         }else{
-            $error = "Your email or password is invalid";
+            $error = "Your Username or password is invalid";
         }
         $stmt->close();
     }
