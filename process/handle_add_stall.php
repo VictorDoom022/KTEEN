8b4b0212eb110aeba41f459f0c098bcee183ecc9
TuanHee <TuanHee0726@github.com>
2019-09-14 00:36:55 +0800

    Update handle_add_stall.php

--- process/handle_add_stall.php
+++ process/handle_add_stall.php
@@ -1,20 +1,12 @@
 <?php
 include '../config/test_input.php';
-$username = '';
-$owner_name = '';
-$stall_name = '';
-$NRIC = '';
-$contact_no = '';
-$email = '';
-$password = '';
 
-$username_valid = '';
-$owner_name_valid = '';
-$stall_name_valid = '';
-$NRIC_valid = '';
-$contact_no_valid = '';
-$email_valid = '';
-$password_valid = '';
+$valid = 'is-valid';
+$invalid = 'is-invalid';
+
+$username = $owner_name = $stall_name = $NRIC = $contact_no = $email = $p = '';
+
+$username_valid = $owner_name_valid = $stall_name_valid = $NRIC_valid = $contact_no_valid = $email_valid = $password_valid = '';
 
 if (isset($_POST['add_stall'])) {
 	$owner_name = test_input($_POST['owner_name']);
@@ -22,34 +14,39 @@ if (isset($_POST['add_stall'])) {
 	$contact_no = test_input($_POST['contact_no']);
 	$email = test_input($_POST['email']);
 
-	$owner_name_valid = 'is-valid';
-	$stall_name_valid = 'is-valid';
-	$NRIC_valid = 'is-valid';
-	$contact_no_valid = 'is-valid';
-	$email_valid = 'is-valid';
+	$username_valid = $owner_name_valid = $stall_name_valid = $NRIC_valid = $contact_no_valid = $email_valid = $valid;
 
 	if($_POST['password'] == $_POST['confirm_password']){
+		$p = test_input($_POST['password']); 
 		$password = md5(test_input($_POST['password']));
-		$password_valid = 'is-valid';
+		$password_valid = $valid;
 	}else{
-		$password_valid = 'is-invalid';
+		$password_valid = $invalid;
 	}
 
-	echo $sql = "SELECT username FROM stall WHERE username = '".test_input($_POST['username'])."' OR stall_name = '".test_input($_POST['stall_name'])."';";
+	$sql = "SELECT username, stall_name FROM stall WHERE username = '".test_input($_POST['username'])."' OR stall_name = '".test_input($_POST['stall_name'])."';";
 	$result = mysqli_query($conn, $sql);
 
-	if(mysqli_num_rows($result) == 0){
-		$username_valid = 'is-valid';
-		$stall_name_valid = 'is-valid';
-	}else{
-		$username_valid = 'is-invalid';
-		$stall_name_valid = 'is-invalid';
+	if(mysqli_num_rows($result) > 0){
+		while ($row = mysqli_fetch_assoc($result)) {
+			if ($row['stall_name'] == $_POST['stall_name']) {
+				$stall_name_valid = $invalid;
+			}
+			if($row['username'] == $_POST['username']){
+				$username_valid = $invalid;
+			}
+		}
+	}
+
+	if($stall_name_valid == $valid){
+		$stall_name = test_input($_POST['stall_name']);
+	}
+	if($username_valid == $valid){
+		$username = test_input($_POST['username']);
 	}
 
 	if($username_valid == 'is-valid' && $owner_name_valid == 'is-valid' && $stall_name_valid == 'is-valid' && $NRIC_valid == 'is-valid' && $contact_no_valid == 'is-valid' && $email_valid == 'is-valid' && $password_valid == 'is-valid'){
 
-		$username = test_input($_POST['username']);
-		$stall_name = test_input($_POST['stall_name']);
 		$owner_image = $username.'_owner.jpg';
 		$stall_image = $username.'_stall.jpg';
 
