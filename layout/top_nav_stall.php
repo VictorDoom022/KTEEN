969ef7bf80bc29e2ef33f1039e436a8b8463e65e
TuanHee <TuanHee0726@github.com>
2019-09-14 00:43:48 +0800

    Update top_nav_stall.php

--- layout/top_nav_stall.php
+++ layout/top_nav_stall.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 switch ($site) {
     case 'Dashboard':
         $icon = '<i class="fas fa-home d-inline-flex mr-2"></i>';
@@ -19,18 +19,18 @@ switch ($site) {
         $icon = '<i class="fas fa-cog d-inline-flex mr-2"></i>';
         break;
 }
- ?>
+?>
 
 <nav class="k-top-nav navbar navbar-expand-lg navbar-light pl-4 col-10 bg-white border-bottom">
     <span class="navbar-brand h1 mb-0 col"><?php echo $icon.$site ?></span>
     <ul class="navbar-nav px-4">
         <li class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <img class="rounded-circle" style="height: 40px;width: 40px;" src="../images/<?= $_SESSION['stall_username'] ?>/<?php echo $_SESSION['kteen_stall_owner_image']; ?>">
                 <?php echo $_SESSION['kteen_stall_name']; ?>
             </a>
             <div class="dropdown-menu dropdown-menu-right rounded-0 shadow border-0" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="">Notification</a>
+                <a class="dropdown-item" href="">Notice</a>
                 <a class="dropdown-item" href="setting.php">Setting</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="index.php?logout='1'">Log Out</a>
