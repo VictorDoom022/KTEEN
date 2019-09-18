a2e62399d986b72e093a4fcc9dc9333beffe221a
TuanHee <TuanHee0726@github.com>
2019-09-18 22:27:37 +0800

    Create handle_add_notice.php

---
+++ process/handle_add_notice.php
@@ -0,0 +1,11 @@
+<?php
+include '../config/test_input.php';
+
+if (isset($_POST['add_notice'])) {
+	$description = test_input($_POST['description']);
+	echo $sql = "INSERT INTO notice(date, stall_ID, description) VALUES (CURDATE(), '".$_SESSION['kteen_stall_id']."', '$description')";
+	mysqli_query($conn, $sql);
+	mysqli_close($conn);
+	header('location: index.php');
+}
+?>
