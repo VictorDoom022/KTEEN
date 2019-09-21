535d4972df012e0093f856225e092ee44ccaab5e
TuanHee <TuanHee0726@github.com>
2019-09-21 20:12:48 +0800

    Create handle_change_password_stall.php

---
+++ process/handle_change_password_stall.php
@@ -0,0 +1,33 @@
+<?php
+include '../config/test_input.php';
+$p = $valid_current_password = $valid_not_match_password = "";
+
+if(isset($_POST['change_password'])){
+	$p = test_input($_POST['current_password']);
+	$current_password = md5($p);
+	$sql = "SELECT * FROM stall WHERE ID = '".$_SESSION['kteen_stall_id']."' AND password = '$current_password';";
+	$result = mysqli_query($conn, $sql);
+	mysqli_num_rows($result);
+	if(mysqli_num_rows($result) == 1){//check account exist
+		$valid_current_password = "is-valid";
+		$new_password = test_input($_POST['new_password']);
+		$retype_new_password = test_input($_POST['retype_new_password']);
+		if($new_password == $retype_new_password){
+			$password = md5($new_password);
+			$sql = "UPDATE stall SET password = '$password' WHERE ID = '".$_SESSION['kteen_stall_id']."'";
+			mysqli_query($conn, $sql);
+			mysqli_close($conn);
+			echo "<script type='text/javascript'>
+					alert('Password change succeeded');
+					location.assign('setting.php');
+				</script>";
+		}else{
+			$valid_not_match_password = 'is-invalid';
+		}
+	}else{
+		$valid_current_password = "is-invalid";
+		$p = "";
+		mysqli_close($conn);
+	}
+}
+?>
