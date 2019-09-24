9b0bbdb4641c01d775c9eee3b7113a50160e0ee2
TuanHee <TuanHee0726@github.com>
2019-09-24 23:44:13 +0800

    Update index.php

--- order_management_system/make_order.php
+++ order_management_system/make_order.php
@@ -15,8 +15,9 @@ include '../config/config.php';
 	<title></title>
 
 	<script src="https://kit.fontawesome.com/baa8fb89d5.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
@@ -85,7 +86,7 @@ include '../config/config.php';
 					<div class="card-footer bg-white">
 						<div class="row">
 							<div class="col-8">
-								<button class="btn btn-block btn-dark">Send Order</button>
+								<button id="send_order_btn" class="btn btn-block btn-dark">Send Order</button>
 							</div>
 							<div class="col-4">
 								<button onclick="window.close()" role="button" class="btn btn-block">Cancel</button>
@@ -147,6 +148,27 @@ include '../config/config.php';
 					"<td>RM "+ total_price +"</td>"+
 					"</tr>"
 				);
+				console.log(typeof orders);
+				console.log(orders);
+			});
+			$('#send_order_btn').click(function() {
+				$.ajax({
+					type: 'POST',
+					url: "../process/handle_add_order.php",
+					data: {order_list: JSON.stringify(orders)},
+					dataType: 'json',
+					success: function(data) {
+						if(data.status = 0){
+							alert(data.msg);
+						}else{
+							alert(data.msg);
+							window.close();
+						}
+					},
+					error: function() {
+						alert("Something error!");
+					}
+				});
 			});
 		});
 	</script>
