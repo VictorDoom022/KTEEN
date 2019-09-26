0c5ba611585854f342e234ba8acc1c5e3fc73744
TuanHee <TuanHee0726@github.com>
2019-09-26 13:29:53 +0800

    Update index.php

--- order_management_system/index.php
+++ order_management_system/index.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+include '../config/config.php';
 include '../process/handle_logout.php';
 include '../process/handle_if_logout_staff.php';
 ?>
@@ -16,8 +17,9 @@ include '../process/handle_if_logout_staff.php';
 	<title></title>
 
 	<script src="https://kit.fontawesome.com/baa8fb89d5.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
@@ -26,25 +28,30 @@ include '../process/handle_if_logout_staff.php';
 	include '../layout/top_nav_staff.php';
 	include '../layout/side_nav_staff.php';
 	?>
-	<main class="row">
-		<div class="col-2"></div>
-		<div class="col-10">
-			<div class="container-fluid pt-3">
+	<main class="container-fluid">
+		<div class="row pt-3">
+			<div class="col-2"></div>
+			<div class="col-10">
 				<div class="k-card card">
-					<div class="card-header bg-white">
+					<div class="card-body">
 						<button onclick="open_add_menu_page()" class="btn btn-dark">Make order</button>
 					</div>
-					<div class="card-body">
-						div
-					</div>
 				</div>
+				<div id="order_area"></div>
 			</div>
 		</div>
+		
 	</main>
 	<script>
 		function open_add_menu_page(){
 			add_menu_window = window.open("make_order.php", "","width=1250, height=500")
 		}
+
+		$(document).ready(function() {
+			var auto_refresh_element = setInterval(function() {
+				$('#order_area').load('order_list.php');
+			}, 1000);
+		});
 	</script>
 </body>
 </html>
