2daec241c026a2dec19239b092cd5f105334d4bc
TuanHee <TuanHee0726@github.com>
2019-09-26 13:29:58 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php 
-$conn = new mysqli('localhost', 'root', '', 'kteen');
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+$conn = mysqli_connect('localhost', 'root', '', 'kteen');
+if (mysqli_connect_errno()) {
+	die("Connection failed: " . mysqli_connect_error());
 }
  ?>
