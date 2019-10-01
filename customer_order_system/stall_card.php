6397e25b233d5d72d5f26c532725aa12a076c789
tuanhee <teotuanhee1yee2@gmail.com>
2019-10-01 13:15:27 +0800

    Update stall_card.php

--- customer_order_system/stall_card.php
+++ customer_order_system/stall_card.php
@@ -1,18 +1,26 @@
 <div class="row">
 	<?php 
-	$sql = "SELECT stall_name FROM stall WHERE status = '1'";
+	$sql = "SELECT username, stall_name FROM stall WHERE status = '1'";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		while ($row = $result->fetch_assoc()) {
+			$stall_username = $row['username'];
 			$stallname = $row['stall_name'];
 	?>
 	<div class="col-sm-6 col-md-4 col-lg-3 p-2">
-	    <a href="menu.php" class="btn btn-block bg-white k-hover-shadow rounded-0" style="box-shadow: 0 0.75rem 1.5rem rgba(18,38,63,.1);">
-	        <!-- <div>
-	            <img src="../images/<?php echo $image; ?>" style="width: 100%;height: 200px;align-self: center;vertical-align: center;">
-	        </div> -->
-            <h5><?php echo $stallname; ?></h5>
-	    </a>
+		<div class="k-card card k-hover-shadow h-100">
+			<div id="<?php echo $ID ?>">
+				<div> 
+					<img src="../images/<?= $stall_username; ?>/stall.jpg" class="items" height="100" alt="" style="width: 100%;height: 200px;align-self: center;vertical-align: center;" />
+				</div>
+				<div class="card-body">
+					<br clear="all" />
+					<div class="card-text">
+						<span class="name h5"><?= $stallname ?></span>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	<?php
 		}
