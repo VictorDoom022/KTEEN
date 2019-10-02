f5ab24a80e5b5145f617ee8f23c6cccb1f037f1e
tuanhee <teotuanhee1yee2@gmail.com>
2019-10-02 22:53:27 +0800

    Update stall_info.php

--- customer_order_system/stall_info.php
+++ customer_order_system/stall_info.php
@@ -11,9 +11,41 @@ if(isset($_POST['stall_username']) && !empty($_POST['stall_username'])){
 <div class="col-md-4 mb-3 text-center">
 	<img style="width: 350px;height: 200px;" src="../images/<?= $row['username']; ?>/stall.jpg">
 </div>
-<div class="col-md-8 bg-white shadow p-4 mb-3">
+<div class="col-md-8 bg-white shadow p-4 mb-3" style="position: relative;">
 	<div class="h5"><?= $row['stall_name']; ?></div>
-	<div class="h6">Status: <?= $r = ($row['status'] == 1)? '<span class="text-success">Opening</span>' :'<span class="text-danger">Closing</span>'; ?></div>
+	<div class="border-top h6 pt-2">
+	Notice**
+	<br>
+	<?php
+	$notice_sql = "SELECT date, description FROM notice WHERE stall_ID = '". $_POST['stall_username'] ."';";
+	$notice = mysqli_query($conn, $notice_sql);
+	if(mysqli_num_rows($notice) > 0){
+		while ($notice_row = mysqli_fetch_assoc($notice)) {
+	?>
+	<div class="row">
+		<div class="col-2 text-muted">
+			<?= $notice_row['date']; ?>
+		</div>
+		<div class="col-10">
+			<?= $notice_row['description']; ?>
+		</div>
+	</div>
+	<?php
+		}
+	}
+	?>
+	
+	
+</div>
+<?php if ($row['status'] == 1){ ?>
+	<span class="bg-success" style="position: absolute;bottom: 10px;right: -16px;width: 120px;height: 32px;transform: skew(-45deg);"></span>
+	<span style="position: absolute;bottom: 10px;right: 0;width: 120px;height: 32px;transform: skew(-45deg);background-color: rgba(0, 255, 0, 0.5);"></span>
+	<span class="text-white" style="position: absolute;bottom: 15px;right: 20px;">Opening</span>
+	<?php }else{ ?>
+		<span class="bg-danger" style="position: absolute;bottom: 10px;right: -16px;width: 120px;height: 32px;transform: skew(-45deg);"></span>
+		<span style="position: absolute;bottom: 10px;right: 0;width: 120px;height: 32px;transform: skew(-45deg);background-color: rgba(255, 0, 0, 0.5);"></span>
+		<span class="text-white" style="position: absolute;bottom: 15px;right: 20px;">Closing</span>
+	<?php } ?>
 </div>
 <?php
 		}
