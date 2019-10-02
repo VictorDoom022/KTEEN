a829a4d2d562f1cb7be42496835248b8703ea50f
tuanhee <teotuanhee1yee2@gmail.com>
2019-10-02 22:53:08 +0800

    Update handle_add_notice.php

--- process/handle_add_notice.php
+++ process/handle_add_notice.php
@@ -3,7 +3,7 @@ include '../config/test_input.php';
 
 if (isset($_POST['add_notice'])) {
 	$description = test_input($_POST['description']);
-	echo $sql = "INSERT INTO notice(date, stall_ID, description) VALUES (CURDATE(), '".$_SESSION['kteen_stall_id']."', '$description')";
+	echo $sql = "INSERT INTO notice(date, stall_ID, description) VALUES (CURDATE(), '".$_SESSION['stall_username']."', '$description')";
 	mysqli_query($conn, $sql);
 	mysqli_close($conn);
 	header('location: index.php');
