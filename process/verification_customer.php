ff0a121d7bf882654e5c5648555020589f369332
tuanhee <teotuanhee1yee2@gmail.com>
2019-10-02 23:39:17 +0800

    Create verification_customer.php

---
+++ process/verification_customer.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include '../config/config.php';
+$error = "";
+if(isset($_POST['login'])){
+    $u = $_POST['username'];
+    $p = ($_POST['password']);
+    echo $u;
+    echo $p;
+    if($stmt = $conn->prepare("SELECT name, password FROM customer WHERE name=? AND password=?")){
+            /*bind parameters for markers*/
+            $stmt->bind_param("ss",$u,$p);
+            $stmt->execute();
+            $stmt->bind_result($username,$password);
+            if($stmt->fetch()){
+                $_SESSION['username'] = $username;
+                $error = "";
+                header('location: index.html');
+            }else{
+                $error = "Your username or password is invalid";
+            }
+            $stmt->close();
+        }
+    $conn->close();
+}
+?>
