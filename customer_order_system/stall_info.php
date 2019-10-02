9faf2d10c379f29cf793c3462b5c8ebff7163258
tuanhee <teotuanhee1yee2@gmail.com>
2019-10-02 21:13:16 +0800

    Update stall_info.php

--- customer_order_system/stall_info.php
+++ customer_order_system/stall_info.php
@@ -1,29 +1,29 @@
 <?php
 include '../config/config.php';
 
-if(isset($_POST['stall_name']) && !empty($_POST['stall_name'])){
-	$stall_name = $_POST['stall_name'];
-	$sql = "SELECT * FROM stall WHERE username = '$stall_name';";
+if(isset($_POST['stall_username']) && !empty($_POST['stall_username'])){
+	$sql = "SELECT username, stall_name, status FROM stall WHERE username = '".  $_POST['stall_username'] ."';";
 	$result = mysqli_query($conn, $sql);
 
 	if (mysqli_num_rows($result) == 1) {
 		while ($row = mysqli_fetch_assoc($result)) {
-			$username = $row['username'];
-			$stall_name = $row['stall_name'];
 ?>
 <div class="col-md-4 mb-3 text-center">
-	<img style="width: 350px;height: 200px;" src="../images/<?= $username ?>/stall.jpg">
+	<img style="width: 350px;height: 200px;" src="../images/<?= $row['username']; ?>/stall.jpg">
 </div>
 <div class="col-md-8 bg-white shadow p-4 mb-3">
-	<div class="h5"><?= $stall_name; ?></div>
+	<div class="h5"><?= $row['stall_name']; ?></div>
+	<div class="h6">Status: <?= $r = ($row['status'] == 1)? '<span class="text-success">Opening</span>' :'<span class="text-danger">Closing</span>'; ?></div>
 </div>
 <?php
 		}
-	}else{
-		
 	}
 }else{
-	header("location : index.html");
+?>
+<script type="text/javascript">
+	window.location.assign("index.html");
+</script>
+<?php
 }
 
 ?>
