768033a886faab8550b0788bf418b6a8ed74a126
tuanhee <teotuanhee1yee2@gmail.com>
2019-10-02 21:12:40 +0800

    Update stall_card.php

--- customer_order_system/stall_card.php
+++ customer_order_system/stall_card.php
@@ -3,7 +3,7 @@ include '../config/config.php';
 
 $search = "";// search stall name by keyword
 if (isset($_POST['search_stall_name'])) {
-	$search = " stall_name LIKE '%". $_POST['search_stall_name'] ."%';";
+	$search = " AND stall_name LIKE '%". $_POST['search_stall_name'] ."%';";
 }
 ?>
 <div class="row">
@@ -17,16 +17,20 @@ if (isset($_POST['search_stall_name'])) {
 	?>
 	<div class="col-sm-6 col-md-4 col-lg-3 p-2">
 		<div class="k-card card k-hover-shadow h-100 stall" style="cursor: pointer;" data-stall="<?= $stall_username; ?>">
-			<div style="position: relative;"> 
+			<div style="position: relative;overflow: hidden;"> 
 				<img src="../images/<?= $stall_username; ?>/stall.jpg" class="items" height="100" alt="" style="width: 100%;height: 200px;align-self: center;vertical-align: center;" />
 				<?php
 				if ($row['status'] == 1) {
 				?>
-				<span class="text-white bg-success px-3 py-1" style="position: absolute;right: 0;bottom: 0;">Opening</span>
+				<span class="bg-success" style="position: absolute;right: 0;bottom: 0;width: 100px;height: 30px;transform: skew(45deg);"></span>
+				<span style="position: absolute;right: 15px;bottom: 0;width: 100px;height: 30px;transform: skew(45deg);background-color: rgba(0, 255, 0, 0.5);"></span>
+				<span class="text-white px-3 py-1" style="position: absolute;right: 12px;bottom: 0;">Opening</span>
 				<?php
 				}else{
 				?>
-				<span class="text-white bg-danger px-3 py-1" style="position: absolute;right: 0;bottom: 0;">Closing</span>
+				<span class="bg-danger" style="position: absolute;right: 0;bottom: 0;width: 100px;height: 30px;transform: skew(45deg);"></span>
+				<span style="position: absolute;right: 15px;bottom: 0;width: 100px;height: 30px;transform: skew(45deg);background-color: rgba(255, 0, 0, 0.5);"></span>
+				<span class="text-white px-3 py-1" style="position: absolute;right: 12px;bottom: 0;">Closing</span>
 				<?php
 				}
 				?>
