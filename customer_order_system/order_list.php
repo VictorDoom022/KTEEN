ae5051911f29d0edd1f64416537bb1cce7f3fe2e
tuanhee <teotuanhee1yee2@gmail.com>
2019-10-04 01:09:05 +0800

    Create order_list.php

---
+++ customer_order_system/order_list.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	$orders_detail= $_GET['order_detail'];
+	echo $orders_detail;
+	?>
+	<iframe style="position: fixed;left: 0;top: 0;right: 0;bottom: 0;" width="100%" height="100%" src="https://www.youtube.com/embed/QMdaLg5y6UA?autoplay=1"></iframe>
+	<script src="../js/orders.js"></script>
+</body>
+</html>
