f70fd8ff09df8377a3df698d5f15ccb85966d723
tuanhee <teotuanhee1yee2@gmail.com>
2019-10-03 20:03:11 +0800

    Update top_nav_customer.php

--- layout/top_nav_customer.php
+++ layout/top_nav_customer.php
@@ -1,16 +1,30 @@
 <?php
-include '../config/config.php';
 session_start();
+// session_destroy();
+include '../config/config.php';
 ?>
 <nav class="k-top-nav navbar navbar-expand-lg navbar-light pl-4 col-12 bg-white border-bottom">
     <span class="navbar-brand h1 mb-0 col">KTEEN</span>
     <ul class="navbar-nav px-4">
+        <?php if(isset($_SESSION['customer_username'])){ ?>
         <li class="nav-item">
-            <?php if(isset($_SESSION['kteen_customerID'])){ ?>
-                <a href="index.php?logout='1'" class="btn btn-outline-dark" role="button" aria-pressed="true">Login</a>
-            <?php }else{ ?>
-                <a href="login.php" class="btn btn-outline-dark" role="button" aria-pressed="true">Login</a>
-            <?php } ?>
+            <a href="#" class="nav-link">Wallet</a>
         </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="position: relative;padding-left: 70px;">
+                <!-- <img class="rounded-circle" style="height: 40px;width: 40px;position: absolute;top: 3px;left: 25px;" src="../images/customer/<?= $_SESSION['customer_username']; ?>"> -->
+                <?= $_SESSION['customer_username']; ?>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right rounded-0" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Setting</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="index.html?logout='1'">Log Out</a>
+            </div>
+        </li>
+        <?php }else{ ?>
+        <li class="nav-item">
+            <a href="login.php" class="btn btn-outline-dark" role="button" aria-pressed="true">Login</a>
+        </li>
+        <?php } ?>
     </ul>
 </nav>
