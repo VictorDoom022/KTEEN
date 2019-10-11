4fc7ec78da535d82203bf89d18e30d8d2a41cf72
tuanhee <teotuanhee1yee2@gmail.com>
2019-10-11 17:40:13 +0800

    update

--- order_management_system/index.php
+++ order_management_system/index.php
@@ -16,7 +16,6 @@ include '../process/handle_if_logout_staff.php';
 
 	<title></title>
 
-	<script src="https://kit.fontawesome.com/baa8fb89d5.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 	<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
