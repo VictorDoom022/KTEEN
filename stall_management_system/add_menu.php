2fd359db2c1ec4313d727a724f707515fe778e6b
tuanhee <teotuanhee1yee2@gmail.com>
2019-11-10 01:12:12 +0800

    icon

--- stall_management_system/add_menu.php
+++ stall_management_system/add_menu.php
@@ -15,6 +15,7 @@ include '../process/handle_add_menu.php';
 
 	<script src="../js/show_input_image.js"></script>
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://kit.fontawesome.com/586e3dfa1f.js" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -86,6 +87,7 @@ include '../process/handle_add_menu.php';
 							</div>
 							<div class="row">
 								<div class="col-12 text-right">
+									<a href="menu.php" class="btn">Cancel</a>
 									<input type="submit" name="add_menu" class="btn btn-dark">
 								</div>
 							</div>
