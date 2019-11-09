aa3d7424cbae2cd4496586639a54aa61e78b9ff7
tuanhee <teotuanhee1yee2@gmail.com>
2019-11-10 01:12:32 +0800

    add function

--- stall_management_system/menu.php
+++ stall_management_system/menu.php
@@ -3,6 +3,9 @@ session_start();
 include '../config/config.php';
 include '../process/handle_logout.php';
 include '../process/handle_if_logout_stall.php';
+include '../process/handle_hide_menu.php';
+include '../process/handle_action_menu.php';
+include '../process/handle_delete_menu.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,12 +14,12 @@ include '../process/handle_if_logout_stall.php';
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
-	<link href="../css/all.css" rel="stylesheet">
 	<!-- Bootstarp CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+	<script src="https://kit.fontawesome.com/586e3dfa1f.js" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<title></title>
@@ -65,7 +68,7 @@ include '../process/handle_if_logout_stall.php';
 						</div>
 					</div>
 				</div>
-				<div id="view"></div>
+				<div class="container-fluid" id="view"></div>
 			</div>
 		</div>
 	</main>
@@ -82,6 +85,7 @@ include '../process/handle_if_logout_stall.php';
 			}
 			live_search();
 		}
+		
 		function live_search(){
 			category = document.getElementById('category').value;
 			var keyword = document.getElementById("keyword").value;
@@ -107,7 +111,7 @@ include '../process/handle_if_logout_stall.php';
 			}else{
 				xhttp.open("GET", "menu_"+ view +".php?c="+category+"&k="+keyword, true);
 				xhttp.send();
-				return
+				return;
 			}
 		}
 	</script>
