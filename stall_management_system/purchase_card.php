ba606c4af556eda508f99b97b2541f90a63ceef0
tuanhee <teotuanhee1yee2@gmail.com>
2019-11-11 22:38:55 +0800

    Create purchase_card.php

---
+++ stall_management_system/purchase_card.php
@@ -0,0 +1,37 @@
+<div class="row">
+	<?php 
+	$stallID = $_SESSION['kteen_stall_id'];
+	$sql = "SELECT * FROM supplier where stall_ID= '$stallID'";
+	$result = $conn -> query($sql);
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+	?>
+	<div class=" col-12 col-md-6 col-lg-4 p-1">
+		<a href="#modal<?php echo $row['ID']; ?>" class="text-decoration-none text-reset" data-toggle="modal">
+			<div class="k-card card k-hover-shadow">
+				<div class="row no-gutters">
+					<!-- <div class="col-4">
+						<img class="rounded-circle p-2" src="../images/staff/<?php echo $row['image']; ?>" style="height: 120px;width: 120px;">
+					</div> -->
+					<div class="col-8">
+						<div class="card-body">
+							<div class="card-title mb-0">
+								<?php echo $row['Name']; ?>
+							</div>
+							<div class="card-text">
+								<small class="text-muted mt-0">
+									<?php echo $row['company_name']; ?>
+								</small>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php include 'purchase_modal.php'; ?>
+	<?php
+		}
+	}
+	?>
+</div>
