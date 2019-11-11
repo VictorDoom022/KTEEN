ba37fc32041348767e5b10bc2c9416ddd548c8d4
tuanhee <teotuanhee1yee2@gmail.com>
2019-11-11 23:37:57 +0800

    Update handle_hide_menu.php

--- process/handle_hide_menu.php
+++ process/handle_hide_menu.php
@@ -2,7 +2,7 @@
 if (isset($_GET['hf'])) {
 	$food_id = $_GET['hf'];
 	$sql = "UPDATE food SET available = '0' WHERE ID = $food_id;";
-	$result = mysqli_query($conn, $sql);
+	mysqli_query($conn, $sql);
 	header("location: menu.php");
 }
 ?>
