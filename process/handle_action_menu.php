7baafb1116c0246231091d2046839c8b2d1f0e24
tuanhee <teotuanhee1yee2@gmail.com>
2019-11-11 23:37:19 +0800

    Update handle_action_menu.php

--- process/handle_action_menu.php
+++ process/handle_action_menu.php
@@ -2,7 +2,7 @@
 if(isset($_GET['af'])){
 	$food_id = $_GET['af'];
 	$sql = "UPDATE food SET available = '1' WHERE ID = $food_id;";
-	$result = mysqli_query($conn, $sql);
+	mysqli_query($conn, $sql);
 	header("location: menu.php");
 }
 ?>
