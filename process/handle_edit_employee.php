28d6a29975beeefc2cc98a0fc2e29c1f5a526796
tuanhee <teotuanhee1yee2@gmail.com>
2019-11-14 01:25:25 +0800

    Create handle_edit_employee.php

---
+++ process/handle_edit_employee.php
@@ -0,0 +1,78 @@
+<?php 
+include '../config/test_input.php';
+
+if(isset($_GET['id'])){
+    $employee_username = test_input($_GET['employee_un']);
+    $sql = "SELECT * FROM staff WHERE username = '$employee_username';";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $food_name = $row['name'];
+            $category_ID = $row['category_ID'];
+            $price = $row['price'];
+            $image = $row['image'];
+        }
+    }
+}
+
+
+if(isset($_POST['add_menu'])){
+    $food_id = test_input($_POST['food_id']);
+    $food_name = $category_ID = $image = $price = "";
+
+    $food_name = test_input($_POST['name']);
+    $category_ID = test_input($_POST['category_ID']);
+    $image = test_input("S" . $_SESSION['kteen_stall_id'] . "_" . $food_name . ".jpg");
+    $image_time = $image . "?t=" . time();
+    $price = test_input($_POST['price']);
+
+    $sql = "UPDATE food SET name = '$food_name', category_ID= '$category_ID', price = '$price', image = '$image_time' WHERE id = '$food_id';";
+    $result = $conn->query($sql);
+
+    $target_dir = "../images/" . $_SESSION['stall_username'] . "/menu/";
+    $target_file = $target_dir.$image;
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["addmenu"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+    // delete file already exists
+    if (file_exists($target_file)) {
+            unlink($target_file.$image);
+            echo 'deleted exists file';
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg") {
+            echo "Sorry, only JPG file are allowed.";
+            $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            echo "<script>alert('Edit Successful')</script>";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+    $conn->close();
+    header('location: menu.php');
+}
+
+?>
