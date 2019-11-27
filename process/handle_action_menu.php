404b71a6b9dee500888f45f23b2fce3051856a7c
tuanhee <teotuanhee1yee2@gmail.com>
2019-11-28 01:32:02 +0800

    injection protect

--- process/handle_action_menu.php
+++ process/handle_action_menu.php
@@ -1,6 +1,7 @@
 <?php
+include '../config/test_input.php';
 if(isset($_GET['af'])){
-	$food_id = $_GET['af'];
+	$food_id = test_input($_GET['af']);
 	$sql = "UPDATE food SET available = '1' WHERE ID = $food_id;";
 	mysqli_query($conn, $sql);
 	header("location: menu.php");
