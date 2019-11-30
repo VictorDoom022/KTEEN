8729678b591394201fbb2e741a9bcdc3a617cf6a
tuanhee <teotuanhee1yee2@gmail.com>
2019-11-30 16:42:03 +0800

    Update order_list.php

--- customer_order_system/order_list.php
+++ customer_order_system/order_list.php
@@ -1,25 +1,17 @@
 <?php
 include '../config/config.php';
-if(isset($_POST['order_list'])){
-	$order_list = json_decode($_POST['order_list'], true);
-	$temp = array();
-	foreach ($order_list as $key => $order_detail) {
-		$food_id = $order_detail['food_id'];
-		$sql = "SELECT name, price FROM food WHERE ID = '$food_id';";
-		$result = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($result) == 1) {
-			while ($row = mysqli_fetch_assoc($result)) {
-				$price = $row['price'];
-				array_push($temp, 
-					array(
-						'food_id' => $order_detail['food_id'], 
-						'food_name' => $row['name'], 
-						'price' => $price, 
-						'quantity' => $order_detail['quantity'], 
-						'remark' => $order_detail['remark']
-					)
+include '../config/test_input.php';
+if(isset($_POST['food_id'])){
+	$food_id = test_input($_POST['food_id']);
+	$sql = "SELECT name, price FROM food WHERE ID = '$food_id';";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) == 1) {
+		while ($row = mysqli_fetch_assoc($result)) {
+			$price = $row['price'];
+			$temp = array(
+					'food_name' => $row['name'], 
+					'price' => $price, 
 				);
-			}
 		}
 	}
 	echo json_encode($temp);
