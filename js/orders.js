6d2be3b79a55afe00d526de06f66eeea053d4ec8
tuanhee <teotuanhee1yee2@gmail.com>
2019-11-30 16:41:56 +0800

    Update orders.js

--- js/orders.js
+++ js/orders.js
@@ -1,27 +1,32 @@
 class Orders{
-	constructor(food_id, quantity, remark){
+	constructor(food_id, name, quantity, price){
 		this.food_id = food_id;
+		this.name = name;
 		this.quantity = quantity;
-		this.remark = remark;
+		this.price = price;
 	}
 
 	get getFood_id(){
 		return this.food_id;
 	}
 
+	get getName(){
+		return this.name;
+	}
+
 	get getQuantity(){
 		return this.quantity;
 	}
 
-	get getRemark(){
-		return this.remark;
-	}
-
-	set getRemark(remark){
-		this.remark  = remark;
+	get getPrice(){
+		return this.price;
 	}
 
 	add_quantity(){
 		this.quantity ++;
+	}
+
+	get getTotalPrice(){
+		return this.price * this.quantity;
 	}
 }
