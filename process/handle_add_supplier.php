55bc1e059e2040c4ebdd71119a5a8d33e6fbc4dc
VictorDoom022 <victordoom022@gmail.com>
2019-12-01 16:35:57 +0800

    Fix handle_add_supplier

--- process/handle_add_supplier.php
+++ process/handle_add_supplier.php
@@ -7,7 +7,7 @@ if (isset($_POST['add_supplier'])) {
 	$contact_no = test_input($_POST['contact_no']);
 	$address = test_input($_POST['address']);
 	$email = test_input($_POST['email']);
-	$sql = "INSERT INTO supplier(name, company_name, contact_no,email, address, stall_ID) VALUES ('$Name', '$company_name','$contact_no' ,'$email','$address', '". $_SESSION['kteen_stall_id'] ."')";
+	$sql = "INSERT INTO supplier(Name, company_name, contact_no,email, address, stall_ID) VALUES ('$Name', '$company_name','$contact_no' ,'$email','$address', '". $_SESSION['kteen_stall_id'] ."')";
 	mysqli_query($conn, $sql);
 	mysqli_close($conn);
 	header('location: purchase.php');
