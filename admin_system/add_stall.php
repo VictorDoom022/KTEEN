e26953e2f2ad0583bc89e223106bd534ab7cb075
tuanhee <teotuanhee1yee2@gmail.com>
2019-12-21 17:43:49 +0800

    add time

--- admin_system/add_stall.php
+++ admin_system/add_stall.php
@@ -1,6 +1,5 @@
 <?php 
 session_start();
-
 include '../config/config.php';
 include '../process/handle_logout.php';
 include '../process/handle_if_logout_admin.php';
@@ -33,11 +32,11 @@ include '../process/handle_add_stall.php';
 		<div class="row py-3">
 			<div class="col-2"></div>
 			<div class="col-10">
-				<div class="k-card card">
-					<form id="add_stall_form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+				<div class="k-card card rounded-0">
+					<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
 						<div class="row no-gutters">
-							<div style="position: relative;width: 100%;height: 250px;">
-								<img src="../images/stall_image.png" style="height: 250px; width: 100%;position: absolute;" id="img-stall">
+							<div style="width: 100%;position: relative;height: 250px;">
+								<img src="../images/stall_image.png" style="height: 250px;width: 100%;position: absolute;" id="#img-stall">
 								<input type="file" name="stall_image" id="input-stall-image" accept="image/*" required style="position: absolute;width: 100%;height: 100%;opacity: 0;" data-target="#img-stall">
 								<label for="input-stall-image" class="btn btn-light" style="position: absolute;right: 10px;bottom: 5px;"><i class="fas fa-camera"></i></label>
 								<div style="position: absolute;top: 50%;left: 3%;">
@@ -51,13 +50,8 @@ include '../process/handle_add_stall.php';
 						</div>
 						<div class="card-body">
 							<div class="row">
-								<div class="col-md-4">
-									<br>
-									<br>
-									<br>
-									<br>
-								</div>
-								<div class="col-md-7">
+								<div class="col-lg-4" style="height: 110px;"></div>
+								<div class="col-lg-7">
 									<div class="card-title h3">
 										Personal Detail
 									</div>
@@ -87,9 +81,9 @@ include '../process/handle_add_stall.php';
 										</div>
 									</div>
 								</div>
-								<div class="col-md-1"></div>
+								<div class="col-lg-1"></div>
 							</div>
-							<div class="col-12 card-title h3">
+							<div class="card-title h3">
 								Account
 							</div>
 							<hr>
@@ -132,6 +126,100 @@ include '../process/handle_add_stall.php';
 									</div>
 								</div>
 							</div>
+							<div class="h3">
+								Opening time
+							</div>
+							<hr>
+							<div class="row">
+								<div class="col-lg-2"></div>
+								<div class="col-lg-7">
+									<div class="form-group row">
+										<label class="col-md-4 col-form-label col-form-label-sm text-md-right">Sunday</label>
+										<div class="col-md-8">
+											<div class="input-group input-group-sm">
+												<input type="time" name="sunday_start" class="form-control" required>
+												<div class="input-group-prepend input-group-append">
+													<span class="input-group-text">-</span>
+												</div>
+												<input type="time" name="sunday_end" class="form-control" required>
+											</div>
+										</div>
+									</div>
+									<div class="form-group row">
+										<label class="col-md-4 col-form-label col-form-label-sm text-md-right">Monday</label>
+										<div class="col-md-8">
+											<div class="input-group input-group-sm">
+												<input type="time" name="monday_start" class="form-control" required>
+												<div class="input-group-prepend input-group-append">
+													<span class="input-group-text">-</span>
+												</div>
+												<input type="time" name="monday_end" class="form-control" required>
+											</div>
+										</div>
+									</div>
+									<div class="form-group row">
+										<label class="col-md-4 col-form-label col-form-label-sm text-md-right">Tuesday</label>
+										<div class="col-md-8">
+											<div class="input-group input-group-sm">
+												<input type="time" name="tuesday_start" class="form-control" required>
+												<div class="input-group-prepend input-group-append">
+													<span class="input-group-text">-</span>
+												</div>
+												<input type="time" name="tuesday_end" class="form-control" required>
+											</div>
+										</div>
+									</div>
+									<div class="form-group row">
+										<label class="col-md-4 col-form-label col-form-label-sm text-md-right">Wednesday</label>
+										<div class="col-md-8">
+											<div class="input-group input-group-sm">
+												<input type="time" name="wednesday_start" class="form-control" required>
+												<div class="input-group-prepend input-group-append">
+													<span class="input-group-text">-</span>
+												</div>
+												<input type="time" name="wednesday_end" class="form-control" required>
+											</div>
+										</div>
+									</div>
+									<div class="form-group row">
+										<label class="col-md-4 col-form-label col-form-label-sm text-md-right">Thursday</label>
+										<div class="col-md-8">
+											<div class="input-group input-group-sm">
+												<input type="time" name="thursday_start" class="form-control" required>
+												<div class="input-group-prepend input-group-append">
+													<span class="input-group-text">-</span>
+												</div>
+												<input type="time" name="thursday_end" class="form-control" required>
+											</div>
+										</div>
+									</div>
+									<div class="form-group row">
+										<label class="col-md-4 col-form-label col-form-label-sm text-md-right">Friday</label>
+										<div class="col-md-8">
+											<div class="input-group input-group-sm">
+												<input type="time" name="friday_start" class="form-control" required>
+												<div class="input-group-prepend input-group-append">
+													<span class="input-group-text">-</span>
+												</div>
+												<input type="time" name="friday_end" class="form-control" required>
+											</div>
+										</div>
+									</div>
+									<div class="form-group row">
+										<label class="col-md-4 col-form-label col-form-label-sm text-md-right">Saturday</label>
+										<div class="col-md-8">
+											<div class="input-group input-group-sm">
+												<input type="time" name="saturday_start" class="form-control" required>
+												<div class="input-group-prepend input-group-append">
+													<span class="input-group-text">-</span>
+												</div>
+												<input type="time" name="saturday_end" class="form-control" required>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-3"></div>
+							</div>
 							<div class="row">
 								<div class="col text-right">
 									<button class="btn text-danger">Cancel</button>
@@ -146,6 +234,17 @@ include '../process/handle_add_stall.php';
 	</main>
 	<script src="../js/show_input_image.js"></script>
 	<script type="text/javascript">
+		// $("input[type='time']").change(function() {
+		// 	var input_name = $(this).attr("name");
+		// 	var time = $(this).val();
+		// 	var arr = input_name.split("_");
+		// 	if (arr[1] == "start") {
+		// 		$("input[name='"+ arr[0] +"_end']").attr("min", time);
+		// 	}else if(arr[1] == "end"){
+
+		// 	}
+		// 	console.log(time);
+		// });
 		$("#input-stall-image").change(function() {
 			readURL(this);
 		});
