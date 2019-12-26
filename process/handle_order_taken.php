67ab6ec0356356f4335efdc70779ece122d7e0fb
tuanhee <teotuanhee1yee2@gmail.com>
2019-12-26 20:46:05 +0800

    add reset number method

--- process/handle_order_taken.php
+++ process/handle_order_taken.php
@@ -1,6 +1,13 @@
 <?php
 if(isset($_GET['taken_id'])){
 	$order_id = $_GET['taken_id'];
+	//reset order number
+	$sql = "SELECT number FROM orders WHERE ID = '$order_id'";
+	$result = mysqli_query($conn, $sql);
+	$order_num = mysqli_fetch_assoc($result)['number'];
+	$sql = "UPDATE number SET status = 'free' WHERE order_number = '$order_num';";
+	mysqli_query($conn, $sql);
+
 	$sql = "UPDATE orders SET taken = '1' WHERE ID = '$order_id';";
 	mysqli_query($conn, $sql);
 	header("location: index.php");
