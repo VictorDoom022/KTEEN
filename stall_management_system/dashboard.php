5dec74e28cad836ad70320cd08712dc700426938
tuanhee <teotuanhee1yee2@gmail.com>
2019-12-27 22:59:34 +0800

    add chart

---
+++ stall_management_system/dashboard.php
@@ -0,0 +1,111 @@
+<?php
+session_start();
+include '../config/config.php';
+
+$stall_ID = $_SESSION['kteen_stall_id'];
+//get current time
+date_default_timezone_set("Asia/Kuala_Lumpur");
+$current_time = time();
+$weekday_today = date('w', $current_time) - 1;
+
+$sql = "SELECT COUNT(ID) AS orders, WEEKDAY(DATE(date)) AS weekday FROM orders WHERE stall_ID = '$stall_ID' AND  DATE > DATE_SUB(DATE(NOW()), INTERVAL DAYOFWEEK(NOW())+6 DAY) AND DATE <= DATE_ADD(DATE(NOW()), INTERVAL DAYOFWEEK(NOW())+1 DAY)  GROUP BY DATE(date);";
+$result = mysqli_query($conn, $sql);
+
+$total = 0;
+
+switch ($weekday_today) {
+	case '0':
+		$weekday_data = array('Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday');
+		$temp = array(1, 2, 3, 4, 5, 6, 0);
+		break;
+	case '1':
+		$weekday_data = array('Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday');
+		$temp = array(2, 3, 4, 5, 6, 0, 1);
+		break;
+	case '2':
+		$weekday_data = array('Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday');
+		$temp = array(3, 4, 5, 6, 0, 1, 2);
+		break;
+	case '3':
+		$weekday_data = array('Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday');
+		$temp = array(4, 5, 6, 0, 1, 2, 3);
+		break;
+	case '4':
+		$weekday_data = array('Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday');
+		$temp = array(5, 6, 0, 1, 2, 3, 4);
+		break;
+	case '5':
+		$weekday_data = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+		$temp = array(6, 0, 1, 2, 3, 4, 5);
+		break;
+	case '6':
+		$weekday_data = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+		$temp = array(0, 1, 2, 3, 4, 5, 6);
+		break;
+}
+
+$count_order = array(0, 0, 0, 0, 0, 0, 0);
+
+if(mysqli_num_rows($result) > 0){
+	$data = array();
+	while ($row = mysqli_fetch_assoc($result)) {
+		array_push($data, $row);
+		$total += $row['orders'];
+	}
+
+	for ($i=0; $i < count($data); $i++) { 
+		$index = array_search($data[$i]['weekday'], $temp);
+		$count_order[$index] = $data[$i]['orders'];
+	}
+}
+?>
+
+<div class="k-card bg-white h-100">
+	<div class="card-body">
+		<div class="h3">Dashboard</div>
+		<hr>
+		<div class="row">
+			<div class="col-6" style="position: relative;">
+				<small class="text-muted">Total Orders of this week</small>
+				<span class="h2 text-center" style="position: absolute;top: 50%; left: 50%;transform: translate(-50%, -50%);"><?= $total; ?></span>
+			</div>
+			<div class="col-6">
+				<canvas id="myChart">Your browser does not support the HTML5 canvas tag.</canvas>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+	var ctx = document.getElementById('myChart').getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: <?php echo json_encode($weekday_data, JSON_NUMERIC_CHECK); ?>,
+	        datasets: [{
+	            label: 'Orders',
+	            data: <?php echo json_encode($count_order, JSON_NUMERIC_CHECK); ?>,
+		        borderColor: 'rbga(0, 0, 0)',
+	        }],
+	    },
+	    options: {
+	    	// responsive: false,
+	        scales: {
+	        	xAxes: [{
+	        		display: false
+	        	}],
+	            yAxes: [{
+                    display: false
+	            }]
+	        },
+	        legend: {
+	        	display: false
+	        }
+	        // elements: {
+	        // 	point: {
+	        // 		radius: 0
+	        // 	}
+	        // }
+	    }
+	});
+</script>
--- stall_management_system/index.php
+++ stall_management_system/index.php
@@ -22,6 +22,14 @@ include '../process/handle_edit_stall_status.php';
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<!-- chart js -->
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css">
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.min.js"></script>
 	<title></title>
 </head>
 <body>
@@ -36,14 +44,7 @@ include '../process/handle_edit_stall_status.php';
 			<div class="col-2"></div>
 			<div class="col-10">
 				<div class="row">
-					<div class="col-md-8 mb-3">
-						<div class="k-card bg-white h-100">
-							<div class="card-body">
-								<div class="h3">Dashboard</div>
-								<hr>
-							</div>
-						</div>
-					</div>
+					<div id="dashboard_area" class="col-md-8 mb-3"></div>
 					<div id="control_panel_area" class="col-md-4 mb-3"></div>
 				</div>
 				<?php include 'notice_panel.php'; ?>
@@ -58,6 +59,7 @@ include '../process/handle_edit_stall_status.php';
 			}
 		}
 		$(document).ready(function() {
+			$('#dashboard_area').load('dashboard.php');
 			$('#control_panel_area').load('control_panel.php');
 			var auto_refresh_element = setInterval(function() {
 				$('#control_panel_area').load('control_panel.php');
