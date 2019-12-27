7d508b7de89528e9ce2ada8a6db2b24c043f8971
tuanhee <teotuanhee1yee2@gmail.com>
2019-12-27 23:13:58 +0800

    add delete employee with image

--- process/handle_delete_employee.php
+++ process/handle_delete_employee.php
@@ -1,8 +1,16 @@
 <?php
 if(isset($_GET['emp_id'])){
 	$employee_id = $_GET['emp_id'];
-	$sql = "DELETE FROM staff WHERE ID = '$employee_id';";
+	$sql = "SELECT username FROM staff WHERE ID = '$employee_id';";
 	$result = mysqli_query($conn, $sql);
+	$staff_username = mysqli_fetch_assoc($result)['username'];
+
+	$sql = "DELETE FROM staff WHERE ID = '$employee_id';";
+	mysqli_query($conn, $sql);
+
+	$target_file = '../images/'. $_SESSION['stall_username'] .'/staff/'. $staff_username .'.jpg';
+	unlink($target_file);
+
 	header('location: employee.php');
 }
 ?>
